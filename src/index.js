9e1af9943edb58dccb32f1c76bc17d51ee4ee43c
M-O-H <mohanishst@gmail.com>
2020-05-03 08:27:20 +0530

    First Component that is Menu Added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Get the Default BootStrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Custom CSS to overwrite the Default BootStrap
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
