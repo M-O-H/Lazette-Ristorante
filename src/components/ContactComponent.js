0e44abcdad4113d7347be7bfbc8c40a06fb3b93c
M-O-H <mohanishst@gmail.com>
2020-05-07 07:30:02 +0530

    16th commit- React-ReduX Form updated

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -19,11 +19,10 @@ class Contact extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    console.log("Current State is: " + JSON.stringify(this.state));
-    alert("Current State is: " + JSON.stringify(this.state));
-    // to prevent redirection
-    event.preventDefault();
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+    // event.preventDefault();
   }
 
   render() {
